dceeeef7c43dd0fb5d875cf3292a00befe764f3d
KARAN <KARAN@KARAN-PC>
2016-07-28 20:23:50 +1200

    e in upper case

---
+++ SmartCalender/src/userbean/Usermaster.java
@@ -0,0 +1,54 @@
+package userbean;
+
+public class Usermaster 
+{
+	private String username = null;
+	private String Password = null;
+	private String name = null;
+	private String Emailid = null;
+	private String adress = null;
+
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	public String getPassword() 
+	{
+		return password;
+	}
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public String getEmailid() 
+	{
+		return emailid;
+	}
+	public void setEmailid(String emailid)
+	{
+		this.emailid = emailid;
+	}
+	public String getUsername() 
+	{
+		
+		return username;
+	}
+	
+	
+
+}
